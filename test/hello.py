1bc8d57dcf3eee5a8cac5ffe4ed00158dced206c
ido555 <13142010+ido555@users.noreply.github.com>
2021-03-14 22:41:41 +0200

    a

--- test/hello.py
+++ test/hello.py
@@ -1,2 +1,12 @@
 # goal: retrieve Car_1 info
-import awscrt.sa
+import boto3
+import json
+
+# initialize client
+from setuptools._distutils.log import warn
+
+iot = boto3.client('iot')
+
+# get current logging levels, format them as JSON, and write them to stdout
+response = iot.get_v2_logging_options()
+print(json.dumps(response, indent=4))
